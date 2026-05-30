c9faeb2076c062fcbb3029cf0a77cb77a9089cd2
nuza <wjdansrud092208@gmail.com>
2026-05-31 01:43:59 +0900

    feat : snippetService todo작섵'

--- src/snippet/snippet.module.ts
+++ src/snippet/snippet.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Snippet } from './snippet.entity';
+import { SnippetService } from './snippet.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Snippet])],
-  exports: [TypeOrmModule],
+  providers: [SnippetService],
+  exports: [TypeOrmModule, SnippetService],
 })
 export class SnippetModule {}
