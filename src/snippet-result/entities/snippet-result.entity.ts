0f61cc848bec521455f581a9c3a8b95250b7b3f5
nuza <wjdansrud092208@gmail.com>
2026-06-01 15:00:37 +0900

    feat : SnippetResult 엔티티 및 모듈 추가

---
+++ src/snippet-result/entities/snippet-result.entity.ts
@@ -0,0 +1,53 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '../../user/user.entity';
+import { Snippet } from '../../snippet/snippet.entity';
+import { ReplayEvent } from '../types/replay-event.interface';
+import { TypoData } from '../types/typo-data.interface';
+
+@Entity()
+export class SnippetResult {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @ManyToOne( () => User, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'userId' })
+    user!: User;
+
+    @Column()
+    userId!: number;
+
+    @ManyToOne( () => Snippet, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'snippetId' })
+    snippet!: Snippet; 
+
+    @Column()
+    snippetId!: number;
+
+    @Column({ type: 'float' })
+    wpm!: number;
+
+    @Column({ type: 'float' })
+    rawWpm!: number;
+
+    @Column({ type: 'float' })
+    accuracy!: number;
+
+    @Column()
+    durationSec!: number;
+
+    @Column({ type: 'jsonb', default: [] })
+    typos!: TypoData[];
+
+    @Column({ type: 'jsonb', default: [] })
+    replayData!: ReplayEvent[];
+
+    @CreateDateColumn()
+    createdAt!: Date;
+}
