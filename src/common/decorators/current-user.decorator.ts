bf276b361ec0e57d7eb524e98f81a4e91533a282
nuza <wjdansrud092208@gmail.com>
2026-06-05 01:00:32 +0900

    fix : CurrentUser HTTP context 검증 추가, null 반환 타입 수정, JwtStrategy 반환 타입 명시

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -2,7 +2,10 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { JwtUser } from '../types/jwt-user.type';
 
 export const CurrentUser = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext): JwtUser => {
-    return ctx.switchToHttp().getRequest().user;
+  (_: unknown, ctx: ExecutionContext): JwtUser | null => {
+    if (ctx.getType() !== 'http') {
+      throw new Error('CurrentUser decorator는 HTTP context 전용입니다.');
+    }
+    return (ctx.switchToHttp().getRequest().user as JwtUser) ?? null;
   },
 );
